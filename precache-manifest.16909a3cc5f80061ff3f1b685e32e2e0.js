0c37ceb0e0276a161b2f1f08dcc5c350df2da719
gppandey123 <gppandey9919@gmail.com>
2020-09-25 15:27:58 +0530

    Updates

---
+++ precache-manifest.16909a3cc5f80061ff3f1b685e32e2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "420b1f47698b40cfed6673f8f2d8806c",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "52961fbfb8214b2699be",
+    "url": "/react-chess/static/css/main.82c4c878.chunk.css"
+  },
+  {
+    "revision": "7f70ff9e4a54f693470a",
+    "url": "/react-chess/static/js/2.82435373.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chess/static/js/2.82435373.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52961fbfb8214b2699be",
+    "url": "/react-chess/static/js/main.db46ac30.chunk.js"
+  },
+  {
+    "revision": "8bac03567591f4b0a1e8",
+    "url": "/react-chess/static/js/runtime-main.ef9098c4.js"
+  }
+]);
